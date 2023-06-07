f61ec6f91cc00881fbc3cfffcdb82144a136af5b
sPalominoSal <82245130+fiddle-leaf@users.noreply.github.com>
2023-06-07 13:26:25 -0400

    Wild Wild Life answered.

--- js-reps-answers.js
+++ js-reps-answers.js
@@ -22,3 +22,49 @@ for (let i = 1; i <= 200; i++) {
         console.log(i);
     }
 }
+
+/**
+ *      *       *   Fizz Buzz    *       *       *       *
+ * This is a classic problem that you should get really comfortable solving. 
+ * If you've solved it before, try to make it more elegant and short.
+ * Write a javascript application that logs all numbers from 1 - 100.
+ *  * If a number is divisible by 3 log "Fizz" instead of the number.
+ *  * If a number is divisible by 5 log "Buzz" instead of the number.
+ *  * If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
+ */
+for (let i = 1; i <= 100; i++){
+    i % 3 == 0 && i % 5 == 0 ? console.log("FizzBuzz") : 
+    i % 3 == 0 ? console.log("Fizz") :
+    i % 5 == 0 ? console.log("Buzz") :
+    console.log(i);
+}
+
+/*      *       *   Wild Wild Life    *       *       *       *
+ * Use the following arrays to answer the questions below: 
+ * You should be modifying the elements by accessing them. 
+ * It is up to you which methods to use.
+ */
+
+//name, species ,age, hometown
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+// Plantee just had her birthday-
+//- change Plantee's array to reflect her being a year older.
+plantee[2]++;
+
+//Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
+wolfy[3] = "Gotham City";
+
+//Give D'Art a second hometown by adding "Hawkins"
+dart[3] = [dart[3], "Hawkins"];
+
+//Porgee decides that Wolfy can't be named "Wolfy" anymore-
+wolfy.shift();
+//-remove "Wolfy" from the wolfyarray and replace it with "Gameboy".
+wolfy.unshift("Gameboy")
+
+console.log(plantee, wolfy, dart[3]);
