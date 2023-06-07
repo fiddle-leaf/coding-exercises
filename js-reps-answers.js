9e1a77fe7ef7a5af0533c44beed303d0e197849e
sPalominoSal <82245130+fiddle-leaf@users.noreply.github.com>
2023-06-07 11:21:22 -0400

    Easy going exercise answered.

---
+++ js-reps-answers.js
@@ -0,0 +1,12 @@
+/**
+ *  *   *   JAVASCRIPT REPS    *    *   *
+ */
+
+/**
+ *      *       *   Easy Going    *       *       *       *
+ * Write a for loop that will log the numbers 1 through 20.
+ */
+console.log('A loop that logs numbers 1 - 20:');
+for (let i = 0; i <= 20; i++){
+    console.log(`#${i}`);
+}
