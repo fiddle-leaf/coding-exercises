8f60a721f1b1dee649c484ae029d0c2e3f3d0bac
sPalominoSal <82245130+fiddle-leaf@users.noreply.github.com>
2023-06-07 16:57:55 -0400

    Methods: Revised completed

--- js-reps-answers.js
+++ js-reps-answers.js
@@ -81,5 +81,74 @@ const ninjaTurtles = ['Donatello', 'Leonardo', 'Raphael', 'Michaelangelo'];
 //Use a for of loop(not a typo - try it out! Try a for ofloop) 
 //to call toUpperCase()on each of them and print out the result.
 for (const ninja of ninjaTurtles) {
-    console.log(ninja.toUpperCase() +"!");
+    console.log(ninja.toUpperCase() + "!");
 }
+
+/*      *       *   Methods: Revisited    *       *       *       *
+ * Do the following and console.log the final results
+ * Note: if you have to add to the array, feel free to add any movie you'd like
+ */
+//Here is a list of favMovies:
+const favMovies = [
+    'Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 
+    'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 
+    'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 
+    'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 
+    'Imitation of Life', 'Snatch', 'Fast and Furious'];
+
+//use the .sortmethod 
+const sortedFavMovies = favMovies.sort();
+//Thought question: what did this do to the array? Did it permanently alter it?
+// yes
+console.log("Did .sort() alter the og array?\t", favMovies === sortedFavMovies ? "Yes" : "No");
+
+//Use the method pop
+favMovies.pop();
+
+//push "Guardians of the Galaxy"
+favMovies.push();
+
+//Reverse the array
+const reversedFavMovies = favMovies.reverse();
+
+//Use the shiftmethod
+favMovies.shift();
+
+//unshift- what does it return?
+//the added item?
+const unshiftedFavMovie = favMovies.unshift();
+console.log(unshiftedFavMovie);             //it actually returns the length of the array
+
+//splice"Django Unchained" and add "Avatar"
+//favMovies.splice(3, 1, "Avatar");
+
+//(try finding the index of "Django Unchained", instead of counting it yourself)
+favMovies.includes("Django Unchained") ? favMovies.splice(favMovies.lastIndexOf("Django Unchained"), 1, "Avatar") 
+: console.log("Movie not here");
+
+//Thought question: did this permanently alter our array?
+// yes?
+console.log(favMovies);                     //og array was changed
+
+//slicethe last half of the array (challenge yourself and try to programatically 
+//determine the middle of the array rather than counting it and hard coding it)
+favMovies.slice(favMovies.length/2)
+//Thought question: did this permanently alter our array?
+// no?
+console.log(favMovies);                     //nope!
+
+//store the value of your slice in a variable, console.log it - 
+let halfFavMovies = favMovies.slice(favMovies.length/2);
+//Thought question: what is going on here?
+//console.log your final results
+console.log('\nslice() method returns a copy of sliced items in array:');
+console.log(halfFavMovies);
+
+//After running the above tasks, console.log the index of "Fast and Furious"
+//-We removed it from the array, what value do we get when we look for the index of something that is not in the array?
+// undefined?
+console.log(favMovies.indexOf('Fast and Furiou')); //-1
+
+//Thought question: that we declared the variable favMovies with const, 
+//and yet, we were allowed to change the array. Weird? Should we have used let?
+console.log('\nconst keyword refers to the immutable reference of an Array, not to its never-changing values.');
