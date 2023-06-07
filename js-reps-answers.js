f901be1a3ef8e6a6a0c18688d517d53e18a496dc
sPalominoSal <82245130+fiddle-leaf@users.noreply.github.com>
2023-06-07 11:39:17 -0400

    Get even answered.

--- js-reps-answers.js
+++ js-reps-answers.js
@@ -10,3 +10,15 @@ console.log('A loop that logs numbers 1 - 20:');
 for (let i = 0; i <= 20; i++){
     console.log(`#${i}`);
 }
+
+/**
+ *      *       *   Get Even    *       *       *       *
+ * Write a for loop that 
+ * logs only the even numbers in 0 through 200.
+ */
+console.log('\nLog only even numbers in 1 - 200:');
+for (let i = 1; i <= 200; i++) {
+    if (i % 2 == 0){
+        console.log(i);
+    }
+}
