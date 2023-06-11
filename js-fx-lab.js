17c9c67c6160ee55f6d439db935c5d81d83a403b
sPalominoSal <82245130+fiddle-leaf@users.noreply.github.com>
2023-06-11 14:02:48 -0400

    Created minusOne fx

---
+++ js-fx-lab.js
@@ -0,0 +1,9 @@
+/**
+ * Write a function called minusOne that takes a parameter num. 
+ * Assuming the argument is a number, print the argument -1.
+ */
+const minusOne = (x) => {
+    return x -=1;
+}
+
+console.log(minusOne(35));
